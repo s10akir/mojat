71dde984fced31eff4aa978ef00f61fa621ab13d
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2018-09-03 10:30:40 +0900

    [new] ChatServerクラス

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 'use strict';
 
+class ChatServer {
+    constructor(http) {
+        this.socket = require('socket.io')(http);
+    }
+
+    start() {
+        this.socket.on('connection', (socket) => {
+            socket.on('Hello', (data) => {
+                console.log('Hello: ' + data);
+            });
+        });
+    }
+}
+
+
+// mainTask
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const PORT = 1067;
+const chatServer = new ChatServer(http);
 
+
+// チャットサーバ起動
+chatServer.start();
+
+// チャットサーバ起動
 app.use(express.static('assets'));
 http.listen(PORT);
